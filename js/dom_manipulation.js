9d84f8acc992df6276104df8fc6fb7fd349ea836
MarcoNaw <166357601+MarcoNawr@users.noreply.github.com>
2024-10-19 23:17:34 +0200

    add: innerHTML &  innerText

--- js/dom_manipulation.js
+++ js/dom_manipulation.js
@@ -8,3 +8,11 @@
 let title = document.getElementById('websiteTitle');
 console.log("Der innerText der Node mit der ID `websiteTitle`: " + title.innerHTML);
 console.log(title);
+
+// Titel ändern
+title.innerHTML = "Neuer Titel"
+
+
+// .innerHTML
+document.getElementById('test_div').innerText = '<h2>Ich bin eine h2</h2>'; // <h2>Ich bin eine h2</h2>
+document.getElementById('test_div').innerHTML = '<h2>Ich bin eine h2</h2>'; // Ich bin eine h2
