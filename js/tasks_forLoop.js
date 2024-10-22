92db1749d9235ee45bfaab91415d2d27bd470a4c
MarcoNaw <166357601+MarcoNawr@users.noreply.github.com>
2024-10-22 23:36:47 +0200

    add: tesks for schleife 3-5

--- js/tasks_forLoop.js
+++ js/tasks_forLoop.js
@@ -53,15 +53,19 @@ printNumbers(3); // Erwartete Ausgabe: 1, 2, 3
 // Aufgabenstellung 3: Rückwärts laufende for-Schleife
 // Aufgabe:
 // Implementiere eine for-Schleife, die die Zahlen von der gegebenen Zahl bis 1 in absteigender Reihenfolge ausgibt.
+function printNumbersReverse(number) {
+    let array = [];
+    for (let i = number; i > 0; i--) {
+        array.push(i);
+    }
+    console.log(array);
+}
+
 // Test:
 console.log("AUSGABE AUFGABE 3:");
 printNumbersReverse(5);
-
 // Erwartete Ausgabe: 5, 4, 3, 2, 1
-
-
 printNumbersReverse(3);
-
 // Erwartete Ausgabe: 3, 2, 1
 
 
@@ -76,15 +80,18 @@ printNumbersReverse(3);
 // Aufgabenstellung 4: Nur jedes dritte Element ausgeben
 // Aufgabe:
 // Implementiere eine for-Schleife, die jedes dritte Element des gegebenen Arrays ausgibt.
+function printEveryThirdElement(array) {
+    for (i = 0; i < array.length; i += 3) {
+        console.log(array[i]);
+    }
+}
+
+
 //Test:
 console.log("AUSGABE AUFGABE 4:");
 printEveryThirdElement([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
 // Erwartete Ausgabe: 1, 4, 7
-
-
 printEveryThirdElement(['a', 'b', 'c', 'd', 'e', 'f']);
-
 // Erwartete Ausgabe: 'a', 'd'
 
 
@@ -97,8 +104,43 @@ printEveryThirdElement(['a', 'b', 'c', 'd', 'e', 'f']);
 // Bonusaufgabe: Prüfung auf Primzahlen
 // Aufgabe:
 // Implementiere eine for-Schleife, um zu überprüfen, ob die gegebene Zahl eine Primzahl ist (d.h. nur durch 1 und sich selbst teilbar ist). Gib true zurück, wenn sie eine Primzahl ist, andernfalls false.
+function isPrime(myInteger) {
+    if (myInteger <= 1) { return false; }
+    if (myInteger == 2) { return true; }
+    for (i = 2; i <= myInteger; i++) {
+        if (myInteger % (i) != 0) {
+            //egal
+        } else if (myInteger != i) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+
 // Test:
 console.log("AUSGABE AUFGABE 5 (Bonus):");
-console.log(isPrime(7)); // Erwartete Ausgabe: true (7 ist eine Primzahl)
+console.log(isPrime(-1) + " -1");   // false
+console.log(isPrime(0) + " 0");     // false
+console.log(isPrime(1) + " 1");     // false
+console.log(isPrime(2) + " 2");    // true
+console.log(isPrime(3) + " 3");    // true
+console.log(isPrime(4) + " 4");    // false
+console.log(isPrime(5) + " 5");    // true
+console.log(isPrime(6) + " 6");    // false
+console.log(isPrime(7) + " 7");    // true
+console.log(isPrime(8) + " 8");    // false
+console.log(isPrime(9) + " 9");    // false
+console.log(isPrime(10) + " 10");  // false
+console.log(isPrime(11) + " 11");  // true
+console.log(isPrime(12) + " 12");  // false
+console.log(isPrime(13) + " 13");  // true
+console.log(isPrime(14) + " 14");  // false
+console.log(isPrime(15) + " 15");  // false
+console.log(isPrime(16) + " 16");  // false
+console.log(isPrime(17) + " 17");  // true
+console.log(isPrime(18) + " 18");  // false
+console.log(isPrime(19) + " 19");  // true
+console.log(isPrime(20) + " 20");  // false
 
-console.log(isPrime(4)); // Erwartete Ausgabe: false (4 ist keine Primzahl)
