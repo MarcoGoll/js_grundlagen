8da8089505c4fbd101487971953fca34fd1d4e81
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2024-11-02 08:58:20 +0100

    add: more examples for object usage

--- js/objects.js
+++ js/objects.js
@@ -9,8 +9,12 @@ let myFancyObject = {
     age: 34,
     height: 175,
     name: "Marco",
-    writeName: function () {
-        console.log("Ich bin eine Funktion des Objectes und schreibe den Namen in die Console : " + this.name);
+    job: {
+        name: 'Koch',
+        Einstieg: '01.01.2000'
+    }, // Object im Object
+    writeName: function (vorwort) {
+        console.log("Ich bin eine Funktion des Objectes und schreibe den Namen in die Console : " + vorwort + " " + this.name);
     }
 }
 
@@ -19,6 +23,30 @@ console.log("Variante 1: Age meines Objekcts: " + myFancyObject.age);
 
 // Variante 2: Zugriff auf Key:Value x vom Object myFancyObject
 console.log("Variante 2: Age meines Objekcts: " + myFancyObject["age"]);
+// typischer Anwendungsfall für diese Schreibweise ist der Zugriff mit Variablen
+let objKey = "age";
+console.log(myFancyObject[objKey]);
 
 // Variante 3: Zugriff auf Funktion y vom Object myFancyObject
-myFancyObject.writeName();
+myFancyObject.writeName("Hallo");
+
+// Variante 4: Zugriff auf Object im Object
+console.log(myFancyObject.job.name);
+
+// Variante 5: Zugriff auf Ganzes Objekt (geht auch als Tabelle)
+console.table(myFancyObject);
+
+//Key's eines Objektes auslesen => Object.Keys() verlangt als Parameter ein Objekt und gibt ein Array aus Strings (mit allen Keys des Objektes) zurück
+console.table(Object.keys(myFancyObject));
+// typischer Anwendungsfall ==> aus Objekt ein Array machen
+let objKeys = Object.keys(myFancyObject);
+let ourArray = [];
+for (let i = 0; i < objKeys.length; i++) {
+    ourArray.push(myFancyObject[objKeys[i]]); // Schreibweise von Variante 2
+}
+console.log("Unser array");
+console.table(ourArray);
+
+
+//Entries eines Objektes auslesen => Object.entries() verlangt als Parameter ein Objekt und gibt ein Array aus Array's zurück die immer jeweils zwei Werte haben (Key und passender Value).
+console.table(Object.entries(myFancyObject));
